81d8ee2b25ec6056c13bb0184bf7607e356d39c5
Rodrigo de Souza <rsouza01@gmail.com>
2024-07-19 08:14:40 +0000

    Scripts refactored

--- startJuliaGenie.sh
+++ startJuliaGenie.sh
@@ -2,25 +2,6 @@
 
 VENV="mana"
 PROJECT="JuliaGeniePoc"
-COMPILED_PROJECT=${PROJECT}-Compiled
-
 JULIA_DEPOT_PATH="/workspaces/julia-genie-poc/local_depot"
 
-julia -e "using Pkg;
-    Pkg.add(\"PackageCompiler\");
-    using PackageCompiler;
-    Pkg.activate(\"${VENV}\";shared=true);
-    println(\">>> DEVELOP <<<\");
-    Pkg.develop(path=\"./${PROJECT}\");
-    println(\">>> INSTANTIATE <<<\");
-    Pkg.instantiate();
-    println(\">>> UPDATE <<<\");
-    Pkg.update();
-    println(\">>> PRECOMPILE <<<\");
-    Pkg.precompile();"
-
-    # create_app(\"./${PROJECT}\", \"./${COMPILED_PROJECT}\", force=true)"
-
-./${PROJECT}/bin/_server mana
-
-# ./${COMPILED_PROJECT}/bin/${PROJECT}
+./start.sh $VENV $PROJECT $JULIA_DEPOT_PATH
--- startJuliaNoGenie.sh
+++ startJuliaNoGenie.sh
@@ -2,25 +2,7 @@
 
 VENV="mana"
 PROJECT="JuliaNoGeniePoc"
-COMPILED_PROJECT=${PROJECT}-Compiled
+# JULIA_DEPOT_PATH="/workspaces/julia-genie-poc/local_depot"
+JULIA_DEPOT_PATH="/workspaces/local_depot"
 
-JULIA_DEPOT_PATH="/workspaces/julia-genie-poc/local_depot"
-
-julia -e "using Pkg;
-    Pkg.add(\"PackageCompiler\");
-    using PackageCompiler;
-    Pkg.activate(\"${VENV}\";shared=true);
-    println(\">>> DEVELOP <<<\");
-    Pkg.develop(path=\"./${PROJECT}\");
-    println(\">>> INSTANTIATE <<<\");
-    Pkg.instantiate();
-    println(\">>> UPDATE <<<\");
-    Pkg.update();
-    println(\">>> PRECOMPILE <<<\");
-    Pkg.precompile();"
-
-    # create_app(\"./${PROJECT}\", \"./${COMPILED_PROJECT}\", force=true)"
-
-./${PROJECT}/bin/_server mana
-
-# ./${COMPILED_PROJECT}/bin/${PROJECT}
+./start.sh $VENV $PROJECT $JULIA_DEPOT_PATH
